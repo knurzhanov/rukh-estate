1b4d8cfdd4d47f04b89d66c82fe77b9a55431efc
knurzhanov <knurzhanov1999@gmail.com>
2024-08-18 20:34:56 +0600

    update

--- src/pages/PropertyList.js
+++ src/pages/PropertyList.js
@@ -22,12 +22,12 @@ const PropertyList = () => {
       setError('');
 
       try {
-        const response = await axios.get('http://localhost:5000/properties', {
-          params: { roomCount: Number(roomCount) },
-        });
-        // const response = await axios.get('https://rukh-estate-api-5571379c698a.herokuapp.com/properties', {
+        // const response = await axios.get('http://localhost:5000/properties', {
         //   params: { roomCount: Number(roomCount) },
         // });
+        const response = await axios.get('https://rukh-estate-api-5571379c698a.herokuapp.com/properties', {
+          params: { roomCount: Number(roomCount) },
+        });
 
         if (Array.isArray(response.data)) {
           const filteredProperties = Number(roomCount) >= 5
