962afbf1160650c0164c7a483523fc28f7c1349e
knurzhanov <knurzhanov1999@gmail.com>
2024-08-19 17:33:57 +0600

    fix

--- src/components/PropertyDetail.js
+++ src/components/PropertyDetail.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { useCurrency } from '../components/CurrencyContext';
 import Slider from 'react-slick';
@@ -14,7 +14,11 @@ const PropertyDetail = () => {
   const [error, setError] = useState('');
   const [conversionRate, setConversionRate] = useState(1);
   const [currentSlide, setCurrentSlide] = useState(1);
+  const navigate = useNavigate();
 
+  const handleBack = () => {
+    navigate(-1); // Перенаправляет на предыдущую страницу
+  };
   useEffect(() => {
     const fetchProperty = async () => {
       try {
@@ -64,6 +68,15 @@ const PropertyDetail = () => {
 
   return (
     <div className="property-detail">
+      <div className='property-first_block'>
+        <a onClick={handleBack}><svg width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="14.5" cy="14.5" r="14.5" fill="#EEEEEE"/>
+<path d="M7.64587 14.6464C7.45061 14.8417 7.45061 15.1583 7.64587 15.3536L10.8278 18.5355C11.0231 18.7308 11.3397 18.7308 11.535 18.5355C11.7302 18.3403 11.7302 18.0237 11.535 17.8284L8.70653 15L11.535 12.1716C11.7302 11.9763 11.7302 11.6597 11.535 11.4645C11.3397 11.2692 11.0231 11.2692 10.8278 11.4645L7.64587 14.6464ZM20.041 14.5L7.99942 14.5V15.5L20.041 15.5V14.5Z" fill="#282828"/>
+</svg>
+
+</a>
+      <h2 className="price">{convertPrice(property.price)}{currency}</h2>
+      </div>
       {imagesToShow.length > 0 ? (
         <div className="image-slider">
          
@@ -90,14 +103,16 @@ const PropertyDetail = () => {
             ))}
             </Fancybox>
           </Slider>
+          <div className="image-counter">
+            {currentSlide} / {imagesToShow.length}
+          </div>
         </div>
       ) : (
         <p>No images available</p>
       )}
       <div className="property-header">
-        <h2 className="price">{convertPrice(property.price)}/месяц</h2>
         <div className="property-location">
-          <p>{property.roomCount} комнаты • {property.area} м² • помесячно • {property.address}</p>
+          <p>{property.roomCount} комнаты • {property.area} м² • {property.address}</p>
         </div>
         <div className="property-location">
           <p>Название ЖК: {property.homeTitle}</p>
