a3a9a126b2ff8fdee10402620b23d02be1a65b55
knurzhanov <knurzhanov1999@gmail.com>
2024-08-20 20:10:57 +0600

    fix

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -5,8 +5,7 @@ import { useCurrency } from './CurrencyContext';
 const PrivateRoute = ({ children, requiredRole }) => {
   const { user, isLoading } = useCurrency();
 
-  console.log('User:', user);
-  console.log('IsLoading:', isLoading);
+
 
   if (isLoading) {
     // Пока данные о пользователе загружаются, показываем индикатор загрузки
@@ -15,13 +14,13 @@ const PrivateRoute = ({ children, requiredRole }) => {
 
   if (!user) {
     // Если пользователь не авторизован, перенаправляем на страницу входа
-    console.log('Redirecting to login...');
+
     return <Navigate to="/" />;
   }
 
   if (requiredRole && user.role !== requiredRole) {
     // Если роль пользователя не соответствует необходимой, перенаправляем на страницу без доступа
-    console.log('Redirecting to non-access...');
+
     return <Navigate to="/non-access" />;
   }
 
