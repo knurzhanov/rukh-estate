55979b68976ed01ddf4b28da59cd9fbda4628a04
knurzhanov <43602553+knurzhanov@users.noreply.github.com>
2025-02-19 16:51:49 +0500

    Update LoginPage.js

--- src/components/auth/LoginPage.js
+++ src/components/auth/LoginPage.js
@@ -39,7 +39,7 @@ const LoginPage = () => {
 
     try {
       // const response = await fetch('http://localhost:5000/api/auth/login', {
-      const response = await fetch('https://rukh-estate-api-5571379c698a.herokuapp.com/api/auth/login', { // Исправленный URL
+      const response = await fetch('https://rukh-ba6f8acf2ce2.herokuapp.com/api/auth/login', { // Исправленный URL
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
